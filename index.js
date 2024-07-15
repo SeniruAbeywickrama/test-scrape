b8b0c989596a893e977ff4efc715901e7c283ed3
Seniru Abeywickrama <142373458+SeniruAbeywickrama-e@users.noreply.github.com>
2024-07-15 10:11:26 +0530

    Port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors= require('cors');
 //For read the user data from sign-up and seller page
 const bodyParser = require('body-parser');
 
+const port = 3001;
+const host = '0.0.0.0';
 
 //seller portal route
 const ScrapeRouter= require('./routes/scrape-router');
@@ -15,7 +17,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-app.listen(3001, ()=>{
+app.listen(port, host,()=>{
     console.log('Service Stared on port 3001');
 })
 
